1b465e96a00ea5fd61510c5fd9d6029a4261b136
Mariusz Cieniewski <mariusz@cieniew.ski>
2018-12-01 11:04:05 +0100

    add jobs and field types

---
+++ src/constants/FIELD_TYPES.js
@@ -0,0 +1,9 @@
+export default {
+  select: "select",
+  number: "number",
+  bool: "bool",
+  dirt: "dirt",
+  textarea: "textarea",
+  text: "text",
+  multiCheckbox: "multiCheckbox",
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,3 @@
 exports.TASK_STATUSES = require("./TASK_STATUSES");
+exports.FIELD_TYPES = require("./FIELD_TYPES");
+exports.JOBS = require("./JOBS");
