610cb53b85b0e45a147c73ef0adf2d492a2d0ddd
Mariusz Cieniewski <mariusz@cieniew.ski>
2018-12-06 17:40:27 +0100

    add roles

---
+++ src/constants/USER_ROLES.js
@@ -0,0 +1,4 @@
+module.exports = {
+  customer: "customer",
+  worker: "worker",
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,4 @@ exports.FIELD_TYPES = require('./FIELD_TYPES');
 exports.JOBS = require('./JOBS');
 exports.NOTIFICATION_TYPES = require('./NOTIFICATION_TYPES');
 exports.EVALUATION_STATUSES = require('./EVALUATION_STATUSES');
+exports.USER_ROLES = require('./USER_ROLES');
